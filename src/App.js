6df07981c905bcaecb726fc987c78da9ef87deb9
Anuj Garg <anuj@Anujs-MacBook-Air.local>
2023-04-12 21:22:29 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Piyush from "./Components/Piyush";
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Piyush name="ABCD"/>
+        <Piyush name="Piyush"/>
+        <Piyush name="Sumit"/>
+        <Piyush name="Anuj"/>
+        <Piyush name="Nadim"/>
+        <Piyush name="Yogesh"/>
     </div>
   );
 }
---
+++ src/Components/Piyush.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Piyush({name}) {
+  return (
+    <div>
+        Hello World!!! Kya Likhu. Or btao {name} ji!!!
+        <br></br>
+    </div>
+  )
+}
